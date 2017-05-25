a8b0c880d74b4832d5f3d49833ab59333936a975
Karen Kiener <karen.kiener@cardinalhealth.com>
2017-05-25 17:53:45 -0400

    changed world to planet

--- src/main/java/com/gitpractice/app/App.java
+++ src/main/java/com/gitpractice/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Planet!" );
     }
 
     //adding a line working in TEST-0001
