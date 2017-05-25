561f2ae9799e482e1d248874258b3e173cc36a30
Karen Kiener <karen.kiener@cardinalhealth.com>
2017-05-25 17:57:11 -0400

    changed planet to sun

--- src/main/java/com/gitpractice/app/App.java
+++ src/main/java/com/gitpractice/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Moon!" );
+        System.out.println( "Hello Sun!" );
     }
 
     //adding a line working in TEST-0001
