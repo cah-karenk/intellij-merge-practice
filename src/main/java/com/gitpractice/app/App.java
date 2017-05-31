7dccae2d52255536b1a8fccae340238f313e9969
Karen Kiener <karen.kiener@cardinalhealth.com>
2017-05-31 11:06:30 -0400

    added next Wednesday and pushing to feature branch

--- src/main/java/com/gitpractice/app/App.java
+++ src/main/java/com/gitpractice/app/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println("This is our added line");
         System.out.println( "Hello Sun!" );
         System.out.println("Goodbye");
-        System.out.println("Wednesday");
+        System.out.println("Changed 'Wednesday' to 'Next Wednesday'");
     }
 
 
