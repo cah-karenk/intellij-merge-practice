cbcf0d5865ab26528d67ffe35e298e0889a8c668
Karen Kiener <karen.kiener@cardinalhealth.com>
2017-05-31 13:25:35 -0400

    changed next Friday to next Thursday - IntelliJ

--- src/main/java/com/gitpractice/app/App.java
+++ src/main/java/com/gitpractice/app/App.java
@@ -4,8 +4,20 @@ package com.gitpractice.app;
  * Hello world!
  *
  */
+<<<<<<< HEAD
 public class App {
     public static void main(String[] args) {
+=======
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("This is our added line");
+        System.out.println( "Hello Sun!" );
+        System.out.println("Goodbye");
+        System.out.println("Changed 'Wednesday' to 'Next Thursday'");
+
+    }
 
         System.out.println("Hello World!");
 
