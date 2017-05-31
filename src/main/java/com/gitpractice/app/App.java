080196e6053f81ad089264bffa7b3af07d0c1c56
Karen Kiener <karen.kiener@cardinalhealth.com>
2017-05-31 13:21:14 -0400

    deleted last system out line

--- src/main/java/com/gitpractice/app/App.java
+++ src/main/java/com/gitpractice/app/App.java
@@ -4,19 +4,11 @@ package com.gitpractice.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("This is our added line");
-        System.out.println( "Hello Sun!" );
-        System.out.println("Goodbye");
-        System.out.println("Changed 'Wednesday' to 'Next Tuesday'");
-        System.out.println("Added this line in IDE after github addition");
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
     }
 
-
-
-    //adding a line working in TEST-0001
-    //adding second comment after changing Planet to Moon in the github editor
 }
